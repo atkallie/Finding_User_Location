2411cb734cfeabb0f05e1c355bfc25da77ff7b0e
atkallie <kal17da@gmail.com>
2017-01-31 13:31:50 -0500

    Initial Commit

--- Finding_User_Location/ViewController.swift
+++ Finding_User_Location/ViewController.swift
@@ -7,12 +7,46 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+//to use the user's location, you need to add a CoreLocation framework under 'Build Phases' tab
+//then edit info.plist file to add 'NSLocationWhileInUseUsageDescription' (reason to want to use user's location) and 'NSAlwaysUsageDescription' (description if you always want to know their description)
+
+//or just add the following to the source code of the info.plist file
+/* 
+<key>NSLocationWhenInUseUsageDescription</key>
+<string>Because I want to know where you are!</string>
+<key>NSLocationAlwaysUsageDescription</key>
+<string>Because I always want to know where you are!</string>
+ */
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //the delegate object to receive update requests
+        locationManager.delegate = self
+        
+        //how accurate you wish for the location to be
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        //request for authorization to retrieve user's location (only while using app)
+        locationManager.requestWhenInUseAuthorization()
+        
+        //start updating location after request is authorized
+        locationManager.startUpdatingLocation()
+        
+    }
+    
+    //how do we actually receive locations?? with this function! (search: 'didUpdateLocations')
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        print(locations)
+        
     }
 
     override func didReceiveMemoryWarning() {
